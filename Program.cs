1ad289b2d0cc1af86f1e6f1555bcca09760d97e8
Dmirty <9048881951@mail.ru>
2022-08-15 10:01:12 +0500

    #3

--- Program.cs
+++ Program.cs
@@ -14,12 +14,23 @@ Console.WriteLine(result);
 */
 
 // #2
-
+/*
 Console.Write($"Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int result = 0;
-for (int i = number; i>=0; i/=10)
+for (int i = number; i>0; i/=10)
 {
-	result = result + number % 10;
+	result = result + i % 10;
 }
-Console.WriteLine($"Сумма цифр составляет: {result}");
+Console.WriteLine($"Сумма цифр числа {number} составляет: {result}");
+*/
+
+Console.WriteLine("Введите размер вводимого массива:");
+int Size_array = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[Size_array];
+
+for (int i = 0; i < Size_array; i++)
+{ array[i] = new Random().Next(10); }
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{ Console.WriteLine($"{array[j]}"); }
